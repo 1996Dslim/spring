b71a73fcdd13f50f1258e02c271a821dd6ba0c32
1996Dslim <961014lds@gmail.com>
2021-07-22 16:49:54 +0900

    chat1

--- src/main/java/com/example/demo/ChatTest.java
+++ src/main/java/com/example/demo/ChatTest.java
@@ -1,5 +1,24 @@
 package com.example.demo;
 
 public class ChatTest {
-	//testtest
+	
+	public static void main(String[] args) {
+		
+		int[] number = {4,30,12,1,45,15,7,8,45,67};
+		int max = number[0];
+		int min = number[0];
+		
+		for(int i  = 0; i < number.length ; i++) {
+			if(number[i] > max) {
+				max = number[i];
+			}
+			else if(number[i] < min) {
+				min = number[i];
+			}
+		}
+		System.out.println(max);
+		System.out.println(min);
+		
+	}
+	
 }
